dde2d3ae5ad29041ed48cf0ca2e50db2348c9f9f
Ben Sevcik <43259346+bsevcik@users.noreply.github.com>
2019-01-21 13:24:54 -0800

    menu collapses after touching outside, box formatting better

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
- $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-                  $("main").toggleClass("slidecontent");
-            });
+$(document).ready(function() {
+           $("main, footer").on("click", function() {
+                 $("nav ul").removeClass("showing");
+                 $("main").removeClass("slidecontent");
+           });
+     });
+$(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+            $("main").toggleClass("slidecontent");
       });
+});
 
 
       // Scrolling Effect
